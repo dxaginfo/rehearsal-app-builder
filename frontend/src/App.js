37a3538af69cff25e908c87d23a2b923db73bf14
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-07-02 19:48:59 -0300

    init simple frontend App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div style={{fontFamily: 'sans-serif'}}>
+      <h1>Rehearsal Scheduler</h1>
+      <p>Automated rehearsal scheduling for bands & musicians.</p>
+    </div>
+  );
+}
+export default App;
